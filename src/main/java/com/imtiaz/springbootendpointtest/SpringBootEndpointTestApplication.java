d4dd1204f2bd4455ee57711437925f560fa7ef8b
imtia <imtiaz005123@gmail.com>
2023-01-20 20:19:22 +0600

    Add dependencies.

---
+++ src/main/java/com/imtiaz/springbootendpointtest/SpringBootEndpointTestApplication.java
@@ -0,0 +1,13 @@
+package com.imtiaz.springbootendpointtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEndpointTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEndpointTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imtiaz/springbootendpointtest/SpringBootEndpointTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imtiaz.springbootendpointtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEndpointTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
